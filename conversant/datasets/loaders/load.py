ad7eb839c1c458fc466d3a588d60d9c71b9e8c1b
shai-meital-altimo <shai@gng.ai>
2020-02-29 21:20:40 +0200

    + function to read conversation data in csv + requirements file

---
+++ conversant/datasets/loaders/load.py
@@ -0,0 +1,43 @@
+import json
+import csv
+import anytree
+import datetime
+import pandas as pd
+import logging
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+
+
+def load2df(path, input_format='csv') -> pd.DataFrame:
+    """
+    :parameter path: path to file
+    :parameter input_format: type of file (string) i.e 'csv', 'json'
+    :returns: pandas data frame
+
+    Reads csv or json file of conversation data, the data should have at least these features
+    'node_id', 'tree_id', 'timestamp', 'author', 'text', 'parent'
+
+    TODOs:
+        1. support reading multiple files from a folder
+        2. support reading of json file
+    """
+    if input_format == 'csv':
+        df = pd.read_csv(path)
+        df = df.filter(['node_id', 'tree_id', 'timestamp', 'author', 'text', 'parent'], axis=1)
+
+        df['index1'] = df.index
+        logging.info(f'Conversation sample has {df.tree_id.nunique()} unique trees')
+        logging.info(f'Conversation sample has {df.author.nunique()} unique authors')
+        logging.info(f"Data is from {datetime.utcfromtimestamp(df.timestamp.min()).strftime('%Y-%m-%d')}")
+        logging.info(f"to {datetime.utcfromtimestamp(df.timestamp.max()).strftime('%Y-%m-%d')}")
+
+    if input_format == 'json':
+        # TODO: add code for reading json to df RON
+        df = None
+
+    return df
+
+
+
+
--- conversant/datasets/loaders/loading.py
+++
@@ -1,7 +0,0 @@
-
-import json
-import csv
-import anytree
-
-def load_cmv(path, format="json") -> AnyTree:
-    pass
