a9978d8c49d152add14e120f4687caad7857d517
ronpik <ronpi1310@gmail.com>
2020-04-23 10:33:40 +0300

    interaction graph filter inplace

--- conversant/interactions/interactions_graph.py
+++ conversant/interactions/interactions_graph.py
@@ -1,4 +1,5 @@
-from typing import Any, Tuple, Dict, NamedTuple, Mapping
+from operator import itemgetter
+from typing import Any, Tuple, Dict, NamedTuple, Mapping, Callable
 
 import networkx as nx
 
@@ -61,7 +62,9 @@ class InteractionsDataDiGraph(nx.DiGraph):
     edge_attr_dict_factory = initialize_pair_interactions_data
 
 
+# type aliases
 InteractionsDict = Dict[Tuple[Any, Any], PairInteractionsData]
+Condition = Callable[[PairInteractionsData], bool]
 
 
 class InteractionsGraph(object):
@@ -76,6 +79,29 @@ class InteractionsGraph(object):
     @property
     def interactions_dict(self) -> InteractionsDict:
         return {(u1, u2): data for u1, u2, data in self.graph.edges(data=True)}
+
+    def filter_users(self, condition: Condition, inplace: bool = False) -> 'InteractionsGraph':
+        """
+        filters author nodes out from the given interactions-graph.
+        Args:
+            condition: a function that receives a `NodeData` as an argument and returns a `bool`
+                       to indicate if to retain an author node or to filter it out.
+            inplace: indicate if to maintain the filtering in the given interaction graph, rather than creating a new object.
+
+        Returns:
+            returns an `InteractionsGraph` filtered from the authors that didn't satisfy the condition.
+        """
+        edges = self.graph.edges(data=True)
+        filtered_pairs_data = [e for e in edges if condition(e[2])]
+        interactions_dict = {e[:2]: e[2] for e in filtered_pairs_data}
+
+        if not inplace:
+            return InteractionsGraph(interactions_dict, self.directed)
+
+        # filter inplace
+        filtered_edges = map(itemgetter(0, 1), filtered_pairs_data)
+        self.__graph = self.__graph.edge_subgraph(filtered_edges)
+        return self
 
     def get_core_interactions(self) -> 'InteractionsGraph':
         pass
--- conversant/interactions/utils/interactions_utils.py
+++ conversant/interactions/utils/interactions_utils.py
@@ -1,28 +1,8 @@
 from operator import itemgetter
 from typing import Callable
 
-from conversant.conversation import NodeData
 from conversant.interactions import InteractionsGraph
 
 PairInteractionsData = dict
-
-Condition = Callable[[PairInteractionsData], bool]
-
-
-def filter_users(interactions: InteractionsGraph, condition: Condition, inplace: bool = False) -> InteractionsGraph:
-    """
-    filters author nodes out from the given interactions-graph.
-    Args:
-        interactions: the interactions graph to filter.
-        condition: a function that receives a `NodeData` as an argument and returns a `bool`
-                   to indicate if to retain an author node or to filter it out.
-        inplace: indicate if to maintain the filtering in the given interaction graph, rather than creating a new object.
-
-    Returns:
-        returns an `InteractionsGraph` filtered from the authors that didn't satisfy the condition.
-    """
-    edges = interactions.graph.edges(data=True)
-    filtered_pairs = [e for e in edges if condition(e[2])]
-
 
 
