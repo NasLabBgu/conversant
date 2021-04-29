faa7492431c4096a60a41c1babeeb911998d6ceb
ronpick <ronpi1310@gmail.com>
2021-04-29 14:35:04 +0300

    detect topic based on wikipedia article - notebook experiment. + indexing and example query

---
+++ conversant/topic/wikitopic.py
@@ -0,0 +1,7 @@
+
+
+class WikiTopic(object):
+    """
+    Detect the topic (or k most probable topics) of a given text, based on the matching to wikipedia articles.
+    """
+    def __init__(self):
